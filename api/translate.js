6d89e4b2b8e179215f508210af8f0d5d5695b21e
GunaNalam <nalam.krishna@tessell.com>
2026-04-25 15:51:28 +0530

    Trigger deployment

--- api/translate.js
+++ api/translate.js
@@ -1,4 +1,5 @@
 // Vercel Serverless Function for translating messages
+// Updated: 2026-04-25
 export default async function handler(req, res) {
     // Enable CORS
     res.setHeader('Access-Control-Allow-Credentials', true);
